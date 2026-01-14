0900f34753907312edeb06fac1b204bb9a3ad424
Fadhili-N <f.lumumba@alustudent.com>
2026-01-14 03:02:46 +0300

    rails store assignment

---
+++ app/controllers/additions_controller.rb
@@ -0,0 +1,14 @@
+class AdditionsController < ActionController::API
+  def create
+    number_one = params[:number_one].to_f
+    number_two = params[:number_two].to_f
+    sum = number_one + number_two
+    
+    render json: {
+      number_one: number_one,
+      number_two: number_two,
+      sum: sum
+    }
+  end
+end
+
